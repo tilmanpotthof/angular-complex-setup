4cdc8463202bbd1ae89c09cbc6b623f798a4ab0a
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-09-26 20:29:46 +0200

    Improves uglify test and add banner generated from gitUtil

--- grunt_modules/uglify.js
+++ grunt_modules/uglify.js
@@ -1,22 +1,45 @@
-module.exports = function() {
+module.exports = function () {
   'use strict';
 
   var _ = require('underscore');
   var config = require('./config/config.js');
+  var gitUtil = require('./config/gitUtil.js');
 
   var uglify = {
   };
 
   var distJsPath = 'generated/dist/js/';
 
-  config.mapModulesWithTemplate('<%= moduleName %>').forEach(function (moduleName) {
-    var minPath = distJsPath + moduleName + '.min.js';
-    var path = distJsPath + moduleName + '.js';
-
+  config.eachModule(function (module, moduleName) {
     uglify[moduleName] = {
+      options: {
+        mangle: false,
+        compress: false,
+        enclose: {
+          angular: 'angular'
+        },
+        beautify: {
+          width: 120,
+          beautify: true,
+          indent_level: 2
+        },
+        banner: gitUtil.banner.generate(module) + '\n'
+      },
       files: {}
     };
-    uglify[moduleName].files[minPath] = [path];
+
+    uglify[moduleName].files[module.dest] = [module.dest];
+
+
+    uglify[moduleName + 'Min'] = {
+      options: {
+        mangle: true,
+        sourceMap: true,
+        banner: gitUtil.banner.generate(module)
+      },
+      files: {}
+    };
+    uglify[moduleName + 'Min'].files[module.minDest] = [module.dest];
   });
 
   return uglify;
